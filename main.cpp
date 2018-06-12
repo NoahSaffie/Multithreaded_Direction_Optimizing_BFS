00f15140f78f3b8baea17f9430208cfc29efeb02
NoahSaffie <noahsaffie98@gmail.com>
2018-06-12 08:26:19 -0400

    Init

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <omp.h>
+#include <vector>
+
+int main(int argc, char* argv[])
+{
+  int alpha;
+  int beta;
+  int edgesFrontier; //m_f
+  int edgesUnexploredNodes; //m_u
+  int verticesFrontier; //n_f
+
+
+    
+  return 0;
+}
+inline void bfs_top_down()
+{
+
+}
+inline void bfs_bottom_up()
+{
+
+}
+inline void bfs_switch()
+{
+
+}
