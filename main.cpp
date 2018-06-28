bce931934888682dd60b558c69d944c3d49d3161
NoahSaffie <noahsaffie98@gmail.com>
2018-06-28 12:27:22 -0400

    Stable, works with ~3x speedup

--- main.cpp
+++ main.cpp
@@ -4,10 +4,11 @@
 #include <queue>
 #include "wtime.h"
 
+#define just_top_down 0
 enum NextToRun { Top_Down, Bottom_Up };
 typedef graph<long, long, int, long, long, char> Graph;
-inline NextToRun bfs_top_down(Graph *ginst, int &edgesFrontier, int &edgesExplored, int* level, std::vector<int> &edges, std::vector<int> &nodes, int current_level, double alpha, const int section_beg, const int section_end);
-inline NextToRun bfs_bottom_up(Graph *ginst, int &edgesFrontier, int &edgesExplored, unsigned num_vertices, int* level, std::vector<int> &edges, std::vector<int> &nodes, int current_level, double beta, const int section_beg, const int section_end);
+inline void bfs_top_down(Graph *ginst, int* level, int* results, int current_level, const int section_beg, const int section_end);
+inline void bfs_bottom_up(Graph *ginst, int* level, int* results, int current_level, const int section_beg, const int section_end);
 
 int main(int argc, char* argv[])
 {
@@ -19,7 +20,7 @@ int main(int argc, char* argv[])
       execv(argv[0], argv);
     }
   //Graph Init
-  std::cout << "Input: ./exe beg csr weight number_threads root(-1 if not sure) (optional)alpha (optional)beta\n";
+  std::cout << "Input: ./exe beg csr weight number_threads root(-1 if not sure) (optional)[alpha, beta]\n";
   if (argc != 6 && argc != 8) { std::cout << "Wrong input\n"; return -1; }
   const char *beg_file = argv[1]; //Vertices -- Like RowPtr
   const char *csr_file = argv[2]; //Edges -- That lead to the Node described from the Node (index+1) in Offsets array (Beg_file)
@@ -34,7 +35,6 @@ int main(int argc, char* argv[])
       alpha = atof(argv[6]);
       beta = atof(argv[7]);
     }
-
   //General Init
   std::vector<int> nodes;
   std::vector<int> edges;
@@ -51,7 +51,7 @@ int main(int argc, char* argv[])
             { level[i] = 0; root = i; break; }
         }
     }
-  printf("Root: %d\n", root);
+  printf("Root: %d\tThis is included in level 0\n", root);
   nodes.at(0) = 1;
   edges.at(0) = (ginst->beg_pos[root+1] - ginst->beg_pos[root]);
   edgesFrontier += edges.at(0);
@@ -68,55 +68,118 @@ int main(int argc, char* argv[])
   int current_level = 0;
   unsigned total_nodes = 0;
   //Main Loop
-
-  #pragma omp parallel num_threads(number_threads) default(none) firstprivate(ginst, alpha, beta, number_threads, total_nodes, num_vertices) shared(level, current_level, edges, nodes, edgesFrontier, edgesExplored, next)
-  {
-    const int tid = omp_get_thread_num();
-    const int section_size = ginst->vert_count/number_threads;
-    const int section_beg = tid*section_size;
-    const int section_end = (tid==number_threads-1 ? ginst->vert_count:section_beg+section_size);
-    while (num_vertices > total_nodes)
-      { //Problem 1: Now that the parallel was moved out, each thread is going to call each function just for itself. Which creates major issues with keeping them "together" and with the same knowledge
-        //Even with criticals, one thread could output its results and then go on to the heuristic before any other thread even got to the outputting of results. Which means, that some threads
-        //Will begin to do different paths since one thread may see lower value when looking at the heuristic than other will, with potential for different returns
-        //"Solution": Add more barriers in this main loop. Technically the last thread out should update "next" to be correct since it should have all the info
-
-        //Problem 2: Top Down is gonna have issues though since it doesn't have to return after each level at the moment. This means the solution to Porblem 1 doesn't help
-        //with Top Down. Another major issue here is that current_level is impossible to keep accurate, without breaking out of function after every level check
-        //Since different threads could each increment the current_level, making it not only way to high, but more importantly creating an issue, where a thread that is still running,
-        //now just had an updated current_level and might start marking levels wrong now. (This could be solved with doing a local copy of current_level, however other issues still remain).
-        //Also creates the issue of the threads branching again. This time one thread may start on another level because it saw the heuristic with a lower value than the others.
-        //"Solution": Break out of function after every single level is explored.
-        //--- While main doesn't have a large amount of costs, this still adds more time ---
-        if(next == Top_Down)
+  int* results = new int[number_threads*3];
+  memset(results, 0, sizeof(int)*number_threads*3); //Results: nodes, edgesExplored, edgesFrontier *number_threads
+  //Just TOP_DOWN
+  if(just_top_down)
+    {
+      #pragma omp parallel num_threads(number_threads) default(none) firstprivate(ginst, alpha, beta, number_threads, total_nodes, num_vertices) shared(level, current_level, edges, nodes, edgesFrontier, edgesExplored, next, results)
+      {
+        const int tid = omp_get_thread_num();
+        const int section_size = ginst->vert_count/number_threads;
+        const int section_beg = tid*section_size;
+        const int section_end = (tid==number_threads-1 ? ginst->vert_count:section_beg+section_size);
+        while(num_vertices > total_nodes)
           {
             #pragma omp barrier
-            next = bfs_top_down(ginst, edgesFrontier, edgesExplored, level, edges, nodes, current_level, alpha, section_beg, section_end);
+            bfs_top_down(ginst, level, &(results[tid*3]), current_level, section_beg, section_end);
             #pragma omp barrier
             #pragma omp single
             {
+              for(int i = 0; i < number_threads; i++)
+                {
+                  nodes.at(current_level) += results[i*3];
+                  edges.at(current_level) += results[i*3+1];
+                  edgesFrontier += results[i*3+2];
+                }
+              edgesExplored += edges.at(current_level);
+              next = (edgesFrontier/(ginst->edge_count-edgesExplored) > alpha) ? Bottom_Up:Top_Down;
               current_level++; nodes.push_back(0); edges.push_back(0);
-              printf("Exited TD\n");
+              memset(results, 0, sizeof(int)*number_threads*3);
             }
-          }
-        if(next == Bottom_Up)
-          {
-            #pragma omp barrier
-            next = bfs_bottom_up(ginst, edgesFrontier, edgesExplored, num_vertices, level, edges, nodes, current_level, beta, section_beg, section_end);
-            #pragma omp barrier
             #pragma omp single
             {
-              current_level++; nodes.push_back(0); edges.push_back(0);
-              printf("Exited BU\n");
+              total_nodes = 0;
+              for(unsigned i = 0; i < nodes.size(); ++i) { total_nodes+= nodes.at(i); } //printf("Total nodes: %d\n", total_nodes);
             }
           }
-        #pragma omp single
-        {
-          total_nodes = 0;
-          for(unsigned i = 0; i < nodes.size(); ++i) { total_nodes+= nodes.at(i); } //printf("Total nodes: %d\n", total_nodes);
-        }
+        #pragma omp cancel parallel
       }
-  }
+    }
+  //Problem 1: Now that the parallel was moved out, each thread is going to call each function just for itself. Which creates major issues with keeping them "together" and with the same knowledge
+  //Even with criticals, one thread could output its results and then go on to the heuristic before any other thread even got to the outputting of results. Which means, that some threads
+  //Will begin to do different paths since one thread may see lower value when looking at the heuristic than other will, with potential for different returns
+  //"Solution": Add more barriers in this main loop. Technically the last thread out should update "next" to be correct since it should have all the info
+
+  //Problem 2: Top Down is gonna have issues though since it doesn't have to return after each level at the moment. This means the solution to Porblem 1 doesn't help
+  //with Top Down. Another major issue here is that current_level is impossible to keep accurate, without breaking out of function after every level check
+  //Since different threads could each increment the current_level, making it not only way to high, but more importantly creating an issue, where a thread that is still running,
+  //now just had an updated current_level and might start marking levels wrong now. (This could be solved with doing a local copy of current_level, however other issues still remain).
+  //Also creates the issue of the threads branching again. This time one thread may start on another level because it saw the heuristic with a lower value than the others.
+  //"Solution": Break out of function after every single level is explored.
+  //--- While main doesn't have a large amount of costs, this still adds more time ---
+
+  //Probelm 3: Two more criticals are now in the code, while they aren't hit too often it is still a problem for performance
+  //Idea(failed): Create one very large results array (num_threads*num_results_per_thread[which is 3 currently]), pass the pointer to the start of the section that belongs to each TID, and use that
+  //When exit function do summing. -- Failed since we need the updated info in the functions.
+  //Branch of idea: No longer check heuristics in the function(s), check in main loop
+  if(!just_top_down)
+    {
+      #pragma omp parallel num_threads(number_threads) default(none) firstprivate(ginst, alpha, beta, number_threads, total_nodes, num_vertices) shared(level, current_level, edges, nodes, edgesFrontier, edgesExplored, next, results)
+      {
+        const int tid = omp_get_thread_num();
+        const int section_size = ginst->vert_count/number_threads;
+        const int section_beg = tid*section_size;
+        const int section_end = (tid==number_threads-1 ? ginst->vert_count:section_beg+section_size);
+        while (num_vertices > total_nodes)
+          {
+            if(next == Top_Down)
+              {
+                #pragma omp barrier
+                bfs_top_down(ginst, level, &(results[tid*3]), current_level, section_beg, section_end);
+                #pragma omp barrier
+                #pragma omp single
+                {
+                  for(int i = 0; i < number_threads; i++)
+                    {
+                      nodes.at(current_level) += results[i*3];
+                      edges.at(current_level) += results[i*3+1];
+                      edgesFrontier += results[i*3+2];
+                    }
+                  edgesExplored += edges.at(current_level);
+                  next = (edgesFrontier/(ginst->edge_count-edgesExplored) > alpha) ? Bottom_Up:Top_Down;
+                  current_level++; nodes.push_back(0); edges.push_back(0);
+                  memset(results, 0, sizeof(int)*number_threads*3);
+                }
+              }
+            if(next == Bottom_Up)
+              {
+                #pragma omp barrier
+                bfs_bottom_up(ginst, level, &(results[tid*3]), current_level, section_beg, section_end);
+                #pragma omp barrier
+                #pragma omp single
+                {
+                  for(int i = 0; i < number_threads; i++)
+                    {
+                      nodes.at(current_level) += results[i*3];
+                      edges.at(current_level) += results[i*3+1];
+                      edgesFrontier += results[i*3+2];
+                    }
+                  edgesExplored += edges.at(current_level);
+                  next = nodes.at(current_level)/num_vertices <  beta ? Top_Down:Bottom_Up;
+                  current_level++; nodes.push_back(0); edges.push_back(0);
+                  memset(results, 0, sizeof(int)*number_threads*3);
+                }
+              }
+            #pragma omp single
+            {
+              total_nodes = 0;
+              for(unsigned i = 0; i < nodes.size(); ++i) { total_nodes+= nodes.at(i); } //printf("Total nodes: %d\n", total_nodes);
+            }
+          }
+        #pragma omp cancel parallel
+      }
+    }
   double end = wtime();
   double time_total = end-start;
   for(unsigned i = 0; i < nodes.size(); ++i)
@@ -134,49 +197,35 @@ int main(int argc, char* argv[])
   free(ginst->weight);
   delete ginst;
   delete[] level;
+  delete[] results;
   return 0;
 }
 
-inline NextToRun bfs_top_down(Graph *ginst, int &edgesFrontier, int &edgesExplored, int* level, std::vector<int> &edges, std::vector<int> &nodes, int current_level, double alpha, const int section_beg, const int section_end)
+inline void bfs_top_down(Graph *ginst, int* level, int* results, int current_level, const int section_beg, const int section_end)
 {
-  int partial_nodes = 0;
-  int partial_edgesExplored = 0;
-  int partial_edgesFrontier = 0;
   for (int vert_index = section_beg; vert_index < section_end; vert_index++) //Go through your section
     {
-        if (level[vert_index]==current_level) // If unexplored
-          {
-            int neighbor_beg = ginst->beg_pos[vert_index];
-            int neighbor_end = ginst->beg_pos[vert_index+1];
-            for(int neighbor_index = neighbor_beg; neighbor_index < neighbor_end; neighbor_index++) //Go through neighbors
-              {
-                int node = ginst->csr[neighbor_index];
-                partial_edgesFrontier += (ginst->beg_pos[node+1] - ginst->beg_pos[node]);
-                ++partial_edgesExplored;
-                #pragma omp critical(NeighborUpdate)
-                if(level[node] == -1) //If neighbor is unexplored
-                  {
-                    level[node] = current_level+1; //Add to next frontier
-                    ++partial_nodes;
-                  }
-              }
-	  }
+      if (level[vert_index]==current_level) // If unexplored
+        {
+          int neighbor_beg = ginst->beg_pos[vert_index];
+          int neighbor_end = ginst->beg_pos[vert_index+1];
+          for(int neighbor_index = neighbor_beg; neighbor_index < neighbor_end; neighbor_index++) //Go through neighbors
+            {
+              int node = ginst->csr[neighbor_index];
+              results[2] += (ginst->beg_pos[node+1] - ginst->beg_pos[node]);
+              ++results[1];
+              #pragma omp critical(NeighborUpdate)
+              if(level[node] == -1) //If neighbor is unexplored
+                {
+                  level[node] = current_level+1; //Add to next frontier
+                  ++results[0];
+                }
+            }
+        }
     }
-  #pragma omp critical(ResultsUpdate) //Stats updating and level update
-  {
-    edges.at(current_level) += partial_edgesExplored;
-    edgesFrontier += partial_edgesFrontier;
-    nodes.at(current_level) += partial_nodes;
-    edgesExplored += edges.at(current_level);
-  }
-  return (edgesFrontier/(ginst->edge_count-edgesExplored) > alpha) ? Bottom_Up:Top_Down;
 }
-inline NextToRun bfs_bottom_up(Graph *ginst, int &edgesFrontier, int& edgesExplored, unsigned num_vertices, int* level, std::vector<int> &edges, std::vector<int> &nodes, int current_level, double beta, const int section_beg, const int section_end)
+inline void bfs_bottom_up(Graph *ginst, int* level, int* results, int current_level, const int section_beg, const int section_end)
 {
-  int partial_nodes = 0;
-  int partial_edgesExplored = 0;
-  int partial_edgesFrontier = 0;
-  edgesFrontier = 0;
   for (int vert_index = section_beg; vert_index < section_end; ++vert_index)
     {
       if (level[vert_index]==-1) //Unexplored (Only want to look through potential children of frontier)
@@ -186,23 +235,15 @@ inline NextToRun bfs_bottom_up(Graph *ginst, int &edgesFrontier, int& edgesExplo
           for (long int neighbor_index = neighbor_beg; neighbor_index < neighbor_end; ++neighbor_index) //Indexes in CSR
             {
               long int node = ginst->csr[neighbor_index]; //Neighbor of vert_index
-              ++partial_edgesExplored;
+              ++results[1];
               if (level[node] == current_level) //Visited neighbor limited to current_level
                 {
                   level[vert_index] = current_level+1;
-                  ++partial_nodes; //Should be 1 for next_level and 0 for previous level (==current_level)
-                  partial_edgesFrontier += (ginst->beg_pos[vert_index+1] - ginst->beg_pos[vert_index]);
+                  ++results[0]; //Should be 1 for next_level and 0 for previous level (==current_level)
+                  results[2] += (ginst->beg_pos[vert_index+1] - ginst->beg_pos[vert_index]);
                   break;
                 }
             }
         }
     }
-  #pragma omp critical(ResultsUpdate) //Required now that each thread is gonna run its own function
-  {
-    nodes.at(current_level) += partial_nodes;
-    edges.at(current_level) += partial_edgesExplored;
-    edgesFrontier += partial_edgesFrontier;
-    edgesExplored += edges.at(current_level);
-  }
-  return nodes.at(current_level)/num_vertices <  beta ? Top_Down:Bottom_Up;
 }
