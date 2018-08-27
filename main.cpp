8ac868860ee4b85e39875c4538a8dd3c52258833
Noah Saffie <noahsaffie98@gmail.com>
2018-08-27 12:49:56 -0400

    Slight Code cleanup - No implementation changes

--- main.cpp
+++ main.cpp
@@ -4,6 +4,17 @@
 #include <queue>
 #include "wtime.h"
 
+/*
+  Author: Noah Saffie
+  Purpose: Implementation of Direction-Optimizing BFS, including the use of OpenMP
+  Date last modified: 8/27/2018
+
+  Additional Notes: 
+  -- The Top Down and Bottom Up BFS functions can be found at the very bottom. I moved parallelization to the highest level I could so those functions are very simple
+  -- Some of the output information (node count per level) is off by a tiny bit when doing multithreaded. This is easily fixed by using a #pragma omp critical but it comes at a high performance cost
+     so I was advised against including it.
+  -- On latest testing with valgrind there were only two "memory leaks" (200 bytes total steak reachable), one is a OpenMP issue (192 bytes) that I can do nothing about, and the other had no tracing information available so was not able to identify the problem (8 bytes). There can exist more that show up based due to OpenMP at times as well, but it is unclear if anything is actually lost or if it is a issue between valgrind, and OpenMP. (I found on StackOverflow this is a common issue, that can not be avoided but is obviouisly very minor here)
+ */
 enum NextToRun { Top_Down, Bottom_Up };
 typedef graph<long, long, int, long, long, char> Graph;
 inline void bfs_top_down(Graph *ginst, int* level, int* results, int current_level, const int section_beg, const int section_end);
@@ -11,6 +22,7 @@ inline void bfs_bottom_up(Graph *ginst, int* level, int* results, int current_le
 
 int main(int argc, char* argv[])
 {
+  //Required for thread cancellation
   if(!omp_get_cancellation())
     {
       char *env = (char*)"OMP_CANCELLATION=true";
@@ -27,8 +39,9 @@ int main(int argc, char* argv[])
   const int number_threads = atoi(argv[4]);
   Graph *ginst = new Graph(beg_file, csr_file, weight_file);
   int root = atoi(argv[5]);
-  double alpha = .07142;
+  double alpha = .07142; //Default values based on the source paper (Scott Beamer "Direction Optimizing BFS")
   double beta = .04166;
+  //If user provided a value, set and use that
   if(atof(argv[6]) == -1)
     {
       alpha = atof(argv[6]);
@@ -37,20 +50,21 @@ int main(int argc, char* argv[])
     {
       beta = atof(argv[7]);
     }
-  int just = atoi(argv[9]);
-  if(just > 2 || just < 0)
+  int style_to_run = atoi(argv[9]);
+  if(style_to_run > 2 || style_to_run < 0)
     {
       std::cout << "Wrong input\n"; return -1;
     }
   int tests_run = atoi(argv[8]);
-  int copy_tests_run = tests_run;
+  int copy_tests_run = tests_run; //Used for calculating the average time at the end
   if(tests_run < 0){ std::cout << "Wrong input\n"; return -1; }
-  //General Init
+  //General Init - Not a great way of dealing with this initialization, but put this way for ease when tests_run > 1
   std::vector<int> init_nodes;
   std::vector<int> init_edges;
   init_nodes.push_back(0); init_edges.push_back(0); //First Level with only root
   unsigned init_num_vertices = 1;
   int init_edgesExplored = 0, init_edgesFrontier = 0;
+  //If the user did not provide a root, we will look for the first valid node number and use that as the root
   if(root == -1)
     {
       for(int i = 0; i < ginst->vert_count; i++)
@@ -64,14 +78,15 @@ int main(int argc, char* argv[])
   init_edges.at(0) = (ginst->beg_pos[root+1] - ginst->beg_pos[root]);
   init_edgesFrontier += init_edges.at(0);
 
-  //Find true number of verticies
+  //Find true number of verticies -- Number of verticles provided by ginst is incorrect since it pads in vertices that were not declared between 0 and the highest vertex id it found
   int last_num = 0;
   for(int j = 1; j < ginst->vert_count; ++j)
     {
       if(ginst->beg_pos[j] > last_num) { last_num = ginst->beg_pos[j]; ++init_num_vertices; }
     }
   //Stats - Init
-  double time_total = 0;
+  double time_total = 0; //Time tracker - double should be plenty of space for almost all parameters (unless of course tests_run is needlessly high AND provided dataset is abnormally large)
+  //While we have more tests to run
   while(tests_run > 0)
     {
       //Reset info for new test run
@@ -89,16 +104,18 @@ int main(int argc, char* argv[])
       int current_level = 0;
       //Main Loop
       //Problem -- Knowing what cache line size line is per user? (Mine is 64 bytes will just work with that)
+      //Note: Padding seemed to provide no increase in performance, however it had no noticeable decrease either, so it will be left in
       int cache_line_size = 64; //Bytes
       int size_int = sizeof(int); // Should be 4 here
       int number_stats = 3;
       int padding = cache_line_size/size_int + 1; //Pad away one full cache size;
       int spacing = cache_line_size/(size_int*number_stats);
       int extra_padding = cache_line_size%(size_int*number_stats);
+      //Necessary to prevent thread overlap when producing values - 
       int* results = new int[padding + (number_threads*(number_stats*spacing+extra_padding))];
-      memset(results, 0, sizeof(int)*(padding + (number_threads*(number_stats*spacing+extra_padding)))); //Results: nodes, edgesExplored, edgesFrontier *number_threads
+      memset(results, 0, sizeof(int)*(padding + (number_threads*(number_stats*spacing+extra_padding)))); //Results: [nodes, edgesExplored, edgesFrontier] repeated for number_threads
       //Just bottom_Up
-      if(just == 2)
+      if(style_to_run == 2)
         {
           #pragma omp parallel num_threads(number_threads) default(none) firstprivate(ginst, alpha, beta, number_threads, num_vertices, padding, spacing, number_stats, extra_padding) shared(level, current_level, edges, nodes, edgesFrontier, edgesExplored, next, results)
           {
@@ -128,7 +145,7 @@ int main(int argc, char* argv[])
           }
         }
       //Just TOP_DOWN
-      if(just == 1)
+      if(style_to_run == 1)
         {
           #pragma omp parallel num_threads(number_threads) default(none) firstprivate(ginst, alpha, beta, number_threads, num_vertices, padding, spacing, number_stats, extra_padding) shared(level, current_level, edges, nodes, edgesFrontier, edgesExplored, next, results)
           {
@@ -158,7 +175,7 @@ int main(int argc, char* argv[])
             #pragma omp cancel parallel
           }
         }
-      if(just == 0)
+      if(style_to_run == 0)
         {
           #pragma omp parallel num_threads(number_threads) default(none) firstprivate(ginst, alpha, beta, number_threads, num_vertices, padding, spacing, number_stats, extra_padding) shared(level, current_level, edges, nodes, edgesFrontier, edgesExplored, next, results)
           {
@@ -212,6 +229,7 @@ int main(int argc, char* argv[])
         }
       double end = wtime();
       time_total+= (end-start);
+      //Only need to output data on last run
       if(tests_run == 1)
         {
           for(unsigned i = 0; i < nodes.size(); ++i)
